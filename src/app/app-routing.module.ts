446f0a209b1c1bc9d64e5e491667aa54ccdc47c8
Maxim Wilman <mwilman@brockhaus-ag.de>
2020-02-24 16:42:24 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PizzaOverviewComponent} from './components/pizza-overview/pizza-overview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pizza', component: PizzaOverviewComponent, pathMatch: 'full'},
+  {path: 'pasta', component: PastaComponent, pathMatch: 'full'},
+  {path: '', redirectTo: 'pizza', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
